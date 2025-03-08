66cfea8908b10075aa125c8c801d037f10e9a994
Sebastian Wirkijowski <sebastian@wirkijowski.dev>
2025-03-08 23:01:17 +0100

    feat(api): updated `requestExtender` middleware.

--- api/src/middleware/requestExtender.ts
+++ api/src/middleware/requestExtender.ts
@@ -1,9 +1,13 @@
 import {NextFunction, Request, Response} from "express";
 import {ulid} from "ulid";
 
+import {statistics} from '@/main';
+
 export default function (req: Request, _: Response, next: NextFunction): void {
 	req.requestId = ulid();
 	req.timestamp = new Date();
+	
+	statistics.requests++;
 	
 	next();
 };
