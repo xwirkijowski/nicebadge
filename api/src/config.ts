f169a1e940df6a7a3f78fa14a4f33aa54ccef681
Sebastian Wirkijowski <sebastian@wirkijowski.dev>
2025-03-12 23:15:24 +0100

    refactor(api): optimised config loader, now exporting both startup timestamp and performance.

--- api/src/config.ts
+++ api/src/config.ts
@@ -1,57 +1,86 @@
 import { globalLogger as log } from "@/utils/log";
 
-export const configTimestamp: number = performance.now();
+export const configTimeStamp: Date = new Date();
+export const configPerformance: number = performance.now();
 
 log.std("Starting NiceBadge instance...");
 
 type TConfigParameter = {
-	name: string,
-	type: string,
+	type: "string" | "number" | "boolean",
 	required?: boolean,
 	default?: string|number|boolean,
-}
+};
 
-const configParameters: TConfigParameter[] = [
-	{
-		name: 'PORT',
-		type: 'number',
+/**
+ * Config parameters as an object.
+ * Trading larger memory usage for faster performance.
+ */
+ const configParameters: Record<string, TConfigParameter> = {
+	PORT: {
+		type: "number",
 		default: 3000,
-	}, {
-		name: 'REDIS_URL',
-		type: 'string',
+	},
+	REDIS_URL: {
+		type: "string",
+	},
+	REDIS_ADDR: {
+		type: "string",
+	},
+	REDIS_PORT: {
+		type: "number",
 	}
-];
+};
 
 const config: Record<string, string|number|boolean> = {};
 
-configParameters.forEach((param) => {
-	if (process.env[param.name]) {
-		let value: string|number|boolean = process.env[param.name]!;
-		value = (param.type === 'boolean')
-			? (value.toLowerCase() === "true")
-			: (param.type === 'number')
-				? Number(value)
-				: value;
-		
-		if (param.type === 'number' && isNaN(value as number)) {
-			log.fatal(`Failed to parse ${param.name} environment variable value, received ${value}, shutting down...`);
-			throw new Error(`Invalid parameter "${param.name}" value, ${value}`);
-		}
-		
-		config[param.name] = value;
-	} else {
-		if (!!param?.required) {
-			log.fatal(`Could not load ${param.name} required environment variable, shutting down...`);
-			process.exit(1);
-		} else if (param?.default !== undefined) {
-			config[param.name] = param.default;
-			log.std(`Optional environment variable ${param.name} not provided, using default (${param.default}).`)
-		} else {
-			log.std(`Optional environment variable ${param.name} not provided.`)
-		}
+/**
+ * Parses environment variables based on the defined config parameters.
+ * Leverages type coercion and validates parsing.
+ *
+ * @param   name    Name of the configuration parameter
+ * @param   param   Configuration parameter
+ * @param   value   Value of the environment variable
+ */
+function parseConfigValue (name: string, param: TConfigParameter, value: string): string|number|boolean {
+	switch (param.type) {
+		case "boolean":
+			return (value.toLowerCase() === "true");
+		case "number":
+			const num: number = Number(value);
+			if (isNaN(num)) {
+				log.fatal(`Failed to parse ${name} environment variable value, received ${value}, shutting down...`);
+				throw new Error(`Invalid parameter "${name}" value, ${value}`);
+			}
+			return num;
+		default:
+			return value;
 	}
-})
+}
 
-log.info(`Finished loading configuration in ${(performance.now() - configTimestamp).toFixed(2)} ms.`)
+/**
+ * Iterate over defined configuration parameters and look up environment variables for values.
+ */
+for (const [name, param] of Object.entries(configParameters)) {
+	const envValue: string|undefined = process.env[name];
+	
+	if (envValue) {
+		config[name] = parseConfigValue(name, param, envValue);
+		continue;
+	}
+	
+	if (param.required) {
+		log.fatal(`Could not load ${name} required environment variable, shutting down...`);
+		process.exit(1);
+	}
+	
+	if (param.default !== undefined) {
+		config[name] = param.default;
+		log.std(`Using default for ${name}: ${param.default}`)
+	} else {
+		log.std(`Optional environment variable ${name} not provided.`)
+	}
+}
+
+log.info(`Config loaded in ${(performance.now() - configPerformance).toFixed(2)} ms.`)
 
 export default config;
