ada3f2c73dfebd2c9acd6940e7781dd4f4637bc9
Sebastian Wirkijowski <sebastian@wirkijowski.dev>
2025-03-13 22:58:34 +0100

    feat(api): extended route handler.

--- api/src/controllers/staticGeneration.controller.ts
+++ api/src/controllers/staticGeneration.controller.ts
@@ -5,9 +5,16 @@ import {globalLogger as log} from "@/utils/log";
 export async function handleRoute (req: FastifyRequest, res: FastifyReply, source: 'params'|'query' = 'params'): Promise<void> {
 	try {
 		const badge = new Badge(req[source]);
+		const badgeMaterialized: string = await badge.materialize()
 		
-		res.code(200).type("image/svg+xml").send(await badge.materialize())
+		if (!badge.materialized) {
+			// @todo: res specific error
+		}
+		
+		res.code(200).type("image/svg+xml").send(badgeMaterialized);
 	} catch (e) {
 		log.error('Something went wrong', e);
+		
+		// @todo: res
 	}
 }
