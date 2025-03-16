548f25e8cd63a54e144663081f49f260571c2e35
Sebastian Wirkijowski <sebastian@wirkijowski.me>
2025-03-16 13:30:29 +0100

    refactor(api): added extra checks to custom icon provider resolve method, changed how data is extracted.

--- api/src/providers/icon/icon.provider.custom.ts
+++ api/src/providers/icon/icon.provider.custom.ts
@@ -15,10 +15,11 @@ export default class CustomIconProvider extends IconProvider {
 	async resolve (icon: Icon): Promise<string> {
 		if (!icon) throw new Error("No icon provided");
 		if (!icon?.encoded) throw new Error("No encoded data provided");
+		if (!icon.encoded.startsWith("data:image/svg")) throw new Error("Malformed or invalid data provided");
 		
 		const encodedData: string = icon.encoded!;
-		const encodedContent: string|undefined = encodedData.split(",")?.pop()?.replaceAll(" ", "+");
-		
+		const encodedContent: string|undefined = encodedData.split(",")?.[1]?.replaceAll(" ", "+");
+
 		if (!encodedContent) throw new Error('Invalid value for custom encoded SVG');
 		
 		const decodedSVG: string = Buffer.from(encodedContent, "base64").toString("utf-8");
